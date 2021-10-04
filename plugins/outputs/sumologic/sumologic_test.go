5cb63c06cd497cb5e76d309c810f9df373e9ebdc
Patryk Małek <pmalek@sumologic.com>
2021-10-04 11:18:43 +0200

    fix: adjust tests for carbon2 serialization changes

--- plugins/outputs/sumologic/sumologic_test.go
+++ plugins/outputs/sumologic/sumologic_test.go
@@ -201,7 +201,7 @@ func TestContentType(t *testing.T) {
 				s.SetSerializer(sr)
 				return s
 			},
-			expectedBody: []byte("metric=cpu field=value  42 0\n"),
+			expectedBody: []byte("metric=cpu field=value  42.000000 0\n"),
 		},
 		{
 			name: "carbon2 (data format = metric includes field) is supported",
@@ -215,7 +215,7 @@ func TestContentType(t *testing.T) {
 				s.SetSerializer(sr)
 				return s
 			},
-			expectedBody: []byte("metric=cpu_value  42 0\n"),
+			expectedBody: []byte("metric=cpu_value  42.000000 0\n"),
 		},
 		{
 			name: "graphite is supported",
@@ -302,7 +302,7 @@ func TestContentEncodingGzip(t *testing.T) {
 				payload, err := io.ReadAll(body)
 				require.NoError(t, err)
 
-				assert.Equal(t, string(payload), "metric=cpu field=value  42 0\n")
+				assert.Equal(t, string(payload), "metric=cpu field=value  42.000000 0\n")
 
 				w.WriteHeader(http.StatusNoContent)
 			})
