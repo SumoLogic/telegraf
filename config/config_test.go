1a5692426c31ac246467e585773db560c8420eec
Patryk Małek <pmalek@sumologic.com>
2022-03-25 14:47:41 +0100

    Remove unneeded outputs

--- config/config_test.go
+++ config/config_test.go
@@ -392,6 +392,7 @@ func TestConfig_WrongFieldType(t *testing.T) {
 }
 
 func TestConfig_InlineTables(t *testing.T) {
+	t.Skip("We don't have output plugins")
 	// #4098
 	t.Setenv("TOKEN", "test")
 
@@ -418,6 +419,7 @@ func TestConfig_SliceComment(t *testing.T) {
 }
 
 func TestConfig_BadOrdering(t *testing.T) {
+	t.Skip("We don't have output plugins")
 	// #3444: when not using inline tables, care has to be taken so subsequent configuration
 	// doesn't become part of the table. This is not a bug, but TOML syntax.
 	c := config.NewConfig()
@@ -432,7 +434,8 @@ func TestConfig_BadOrdering(t *testing.T) {
 
 func TestConfig_AzureMonitorNamespacePrefix(t *testing.T) {
 	// #8256 Cannot use empty string as the namespace prefix
-	c := config.NewConfig()
+	t.Skip("We don't have output plugins")
+	c := NewConfig()
 	require.NoError(t, c.LoadConfig("./testdata/azure_monitor.toml"))
 	require.Len(t, c.Outputs, 2)
 
