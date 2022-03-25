c4c5059383696d3c9cfacecc30dcf4b5aa592b8f
Patryk Małek <pmalek@sumologic.com>
2022-03-25 14:53:19 +0100

    Remove unneeded processors

--- config/types_test.go
+++ config/types_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/config"
 	"github.com/influxdata/telegraf/plugins/inputs"
-	"github.com/influxdata/telegraf/plugins/processors/reverse_dns"
 )
 
 func TestConfigDuration(t *testing.T) {
